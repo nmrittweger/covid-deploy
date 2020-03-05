5390a416ebb6141e69d0031e3373c3dd6528fb22
nmrittweger <56150613+nmrittweger@users.noreply.github.com>
2020-03-05 15:33:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash()
 
 server = app.server
 
@@ -21,4 +20,4 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
