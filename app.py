7e435b573f5f658c97865aa47aea912df7f2eab4
nmrittweger <56150613+nmrittweger@users.noreply.github.com>
2020-03-05 15:22:53 +0100

    updating test env

--- app.py
+++ app.py
@@ -1,49 +1,24 @@
-# Import required libraries
-import os
-from random import randint
-
-
-import flask
 import dash
-
 import dash_core_components as dcc
 import dash_html_components as html
 
-
-# Setup the app
-# Make sure not to change this file name or the variable names below,
-# the template is configured to execute 'server' on 'app.py'
-server = flask.Flask(__name__)
-server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(__name__, server=server)
-
-
-# Put your Dash code here
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+server = app.server
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+top_markdown_text = '''
+### Dash Boilerplate
+This template is on the `boilerplate` branch of the tutorial [Github repo](https://github.com/zwrankin/dash_tutorial)  
+See the `master` branch to build an interactive visualization app
+'''
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
+app.layout = html.Div([
+
+    dcc.Markdown(children=top_markdown_text),
+
 ])
 
-# Run the Dash app
 if __name__ == '__main__':
-    app.server.run(debug=True, threaded=True)
+    app.run_server(debug=False)
